62a73d427a9e40e4d3b3329c3335661baa7961ce
AlandisAyupov <steamuservni@gmail.com>
2023-11-04 16:13:32 -0400

    third_commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import Header from "./components/Header/Header.js";
 
 function App() {
-  printWeatherData = () => {
+  const printWeatherData = () => {
     fetch(
       "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={f7b01ad7a4c6639b767e686b3284b139}"
     )
@@ -9,7 +10,12 @@ function App() {
       .then((data) => console.log(data))
       .catch((error) => console.log("ERROR"));
   };
-  return <button> Weather Data </button>;
+  return (
+    <div>
+      <Header />
+      <button onClick={printWeatherData}>Print Weather</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import style from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Weather App</h1>
+    </div>
+  );
+};
+
+export default Header;
