8d1b9343e3c7e5874a846698c755b3ac20e30b99
AlandisAyupov <steamuservni@gmail.com>
2023-11-04 15:59:58 -0400

    second_commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  printWeatherData = () => {
+    fetch(
+      "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={f7b01ad7a4c6639b767e686b3284b139}"
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log("ERROR"));
+  };
+  return <button> Weather Data </button>;
+}
+
+export default App;
