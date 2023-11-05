239d2225d19e55a82fef8ec6843bbf39763c941f
AlandisAyupov <steamuservni@gmail.com>
2023-11-04 21:24:13 -0400

    seventh_commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header.js";
 import { useEffect, useState } from "react";
+import background from "./images/sky.jpg";
 
 function App() {
   const [weather, setWeather] = useState(null);
@@ -19,10 +20,39 @@ function App() {
       .catch((error) => console.log("ERROR"));
   };
   return (
-    <div class="context">
-      {weather && <h1>{weather.weather}</h1>}
-      <button onClick={printWeatherData}>Print Weather</button>
+    <div>
       <Header />
+      <div class="box1">
+        <button onClick={printWeatherData}>Print Weather</button>
+        <h3>Latitude:</h3>
+        <input
+          type="text"
+          onChange={(e) => setLat(e.target.value)}
+          value={lat}
+        />
+        <h3>Longitude:</h3>
+        <input
+          type="text"
+          onChange={(e) => setLon(e.target.value)}
+          value={lon}
+        />
+      </div>
+      <div class="box2">
+        {weather && (
+          <div>
+            {Object.keys(weather.main).map((key, index) => {
+              return (
+                <div key={index}>
+                  <h2>
+                    {key}: {weather.main[key]}
+                  </h2>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
