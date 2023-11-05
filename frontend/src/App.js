85ee61df6b7fd0c2c059d11ffe31d0ce89d6bee0
AlandisAyupov <steamuservni@gmail.com>
2023-11-04 20:20:24 -0400

    sixth_commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,28 @@
 import Header from "./components/Header/Header.js";
-import earth from "./images/Earth.jpg";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [weather, setWeather] = useState(null);
+  const [lat, setLat] = useState(0);
+  const [lon, setLon] = useState(0);
+
   const printWeatherData = () => {
+    const api = "f7b01ad7a4c6639b767e686b3284b139";
     fetch(
-      "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={f7b01ad7a4c6639b767e686b3284b139}"
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}`
     )
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        setWeather(data);
+        console.log(data);
+      })
       .catch((error) => console.log("ERROR"));
   };
   return (
     <div class="context">
-      <Header />
+      {weather && <h1>{weather.weather}</h1>}
       <button onClick={printWeatherData}>Print Weather</button>
+      <Header />
     </div>
   );
 }
